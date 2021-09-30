26942c08112acaf6df4c75b56bba0ba4670b7d51
Jeffrey <1999jeffreynijkamp@gmail.com>
2021-09-30 10:29:53 +0200

    update filters

--- src/javaScript/pages/comics.js
+++ src/javaScript/pages/comics.js
@@ -18,13 +18,39 @@ $(document).ready(function() {
   $('.checkBox').click(function() {
     let cat = $(this).find('p');
     cat = cat.html();
-    $('.comicWrap').each(function(){
-      if($(this).hasClass(cat)){
-        $(this).addClass('active');
+    let filterArr = [];
+    $('.checkBox').each(function() {
+      const box = $(this);
+      if($(this).find('input').is(":checked"))
+      {
+        const char = box.find('p').html();
+        filterArr.push(char);
       }
-      else{
-        $(this).addClass('close');
+      else
+      {
+        const char = box.find('p').html();
+        for(let i = 0; i < filterArr.length; i++)
+        {
+          if(filterArr[i] === char)
+          {
+            filterArr.splice(i, 1);
+          }
+        }
       }
     });
+    //https://www.w3schools.com/js/tryit.asp?filename=tryjs_ajax_post2
+    //https://stackoverflow.com/questions/9001526/send-array-with-ajax-to-php-script
+    // console.log(filterArr);
+    // console.log("_____________");
+    if($(this).find('input').is(":checked")){
+      $('.comicWrap').each(function(){
+        if($(this).hasClass(cat)){
+          $(this).addClass('active');
+        }
+        else{
+          $(this).addClass('close');
+        }
+      });
+    }
   })
 });
