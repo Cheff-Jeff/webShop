277c1e44b60ba600ebaf48c6e0f5f17eb476df9a
Jeffrey <1999jeffreynijkamp@gmail.com>
2021-10-06 14:58:12 +0200

    update comic

--- pages/comic.php
+++ pages/comic.php
@@ -8,13 +8,56 @@
     include_once($root."/src/php/pages/comic.php");
     include_once($root."/pages/templates/top.php");
 ?>
-    <section>
-        <div id="<?=$id?>" class="comic">
-            <img src="<?=$img1?>" style="max-width: 150px;">
-            <span><?=$txt1?></span>
-            <a id="btnAddToCard" href="#">add to card</a>
+<section class="banner">
+    <div class="txtWrap">
+        <h1><?=$title?></h1>
+    </div>
+    <div class="imgWrap">
+        <img src="<?=$bannerImg?>">
+    </div>
+</section>
+<section class="content">
+    <div class="background" style="background-image: url(<?=$img1?>);">
+    </div>
+    <div id="<?=$id?>" class="comic">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="img-wrap">
+                        <img src="<?=$img1?>">
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="info">
+                        <h2><?=$txt1?></h2>
+                        <div class="info-block">
+                            <p>Writer:</p>
+                            <p><?=$writer?></p>
+                        </div>
+                        <div class="info-block">
+                            <p>Publisher:</p>
+                            <p><?=$Publisher?></p>
+                        </div>
+                        <div class="info-block">
+                            <p>Bundel:</p>
+                            <p><?=$bundel?></p>
+                        </div>
+                        <div class="info-block">
+                            <p>Price:</p>
+                            <p><?=$price?></p>
+                        </div>
+                        <a id="btnAddToCard" href="#">add to card</a>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="desc">
+                        <?=$txt2?>
+                    </div>
+                </div>
+            </div>
         </div>
-    </section>
+    </div>
+</section>
 <?php 
     include_once($root."/pages/templates/bottom.php");
 ?>
--- src/php/pages/comic.php
+++ src/php/pages/comic.php
@@ -9,6 +9,57 @@ $slug = getSlug($request);
 $link = $comicApi.$slug;
 $comic = getData($link);
 
+$bannerImg = $host.'wordpress/wp-content/uploads/2021/09/secBanner.png';
+
 $img1 = $comic[0]['imageURL']['large'];
 $txt1 = $comic[0]['title']['rendered'];
+$txt2 = $comic[0]['content']['rendered'];
 $id = $comic[0]['id'];
+$writer = $comic[0]['acf']['writer'];
+$price = str_replace('.',',',$comic[0]['acf']['price']);
+$bundel = $comic[0]['acf']['bundel'];
+$Publisher;
+$title;
+
+$catParent;
+$pubCat;
+$first = false;
+$second = false;
+foreach($comic[0]['categoryName'] as $cat)
+{
+    if($cat['slug'] == 'characters')
+    {
+        $catParent = $cat['cat_ID'];
+        $first = true;
+    }
+    else if($cat['slug'] == 'publisher')
+    {
+        $pubCat = $cat['cat_ID'];
+        $second = true;
+    }
+    if($first && $second)
+    {
+        $first = false;
+        $second = false;
+        break;
+    }
+}
+foreach($comic[0]['categoryName'] as $cat)
+{
+    if($cat['category_parent'] == $catParent)
+    {
+        $title = $cat['cat_name'];
+        $first = true;
+    }
+    else if($cat['category_parent'] == $pubCat)
+    {
+        $Publisher = $cat['cat_name'];
+        $second = true;
+    }
+    if($first && $second)
+    {
+        $first = false;
+        $second = false;
+        break;
+    }
+}
